13119c13a835414b290ff31a29b1d9db30a247b1
Adam Januszewski <Adpjan@att.net>
2022-05-10 10:21:38 -0400

    Initial Commit

---
+++ Devotion/DevotionApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevotionApp.swift
+//  Devotion
+//
+//  Created by adam janusewski on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DevotionApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
